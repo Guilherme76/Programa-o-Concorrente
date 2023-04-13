30acff52df3a2bdf5adc5355a42bb0a82da3aa7e
Guilherme76 <49200440+Guilherme76@users.noreply.github.com>
2023-04-13 09:39:30 -0300

    atv4
    atv4

--- Atv4.c
+++ Atv4.c
@@ -1,86 +1,102 @@
-#include <stdio.h>
 #include <omp.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define num_steps 300000000
+#define MAX 100
+#define TAMANHO 100000000
+
+double *gerar_vetor(int n);
+int *gerar_vetor_inteiro(int n);
+void mostrar_vetor_inteiro(int *v,int tamanho);
+void mostrar_vetor(double *v,int tamanho);
 
 int main() {
-    //static long num_steps = 100000000;
-    
-    double step, x, pi, sum = 0.0;;
-    int i;
-    int t = omp_get_num_threads();
     /*
-    VERSÃO SEQUENCIAL
+    INICIO
     */
-    printf("Threads: %d\n",t);
+    time_t t;
+    srand(time(NULL));
+    int *vetor = NULL;
+    vetor = gerar_vetor_inteiro(TAMANHO);
+
+
     double inicio = omp_get_wtime();
-    step = 1.0/(double) num_steps;
-    for (i=0;i< num_steps; i++){
-        x = (i + 0.5) * step; // Largura do retângulo
-        sum = sum + 4.0 / (1.0 + x*x); // Sum += Área do retângulo
+
+    int soma = 0;
+    int i = 0;
+    int num = 100000000;
+    for(i = 1; i < num; i++) {
+        if((num % i) == 0) {
+            soma = soma + i;
+        }
     }
-    pi = step * sum;
     double fim = omp_get_wtime();
     double t_serial = fim-inicio;
-    printf("%.5f\n",pi);
-    printf("Execucao sequencial(s): %f\n",t_serial); 
-    
-    /*
-    VERSÃO PARALELA
-    */
+    printf("soma dos divisores igual à %d", soma);
+    printf("\nExecucao sequencial(s): %f\n",t_serial); 
 
-    /*
-    x = 0;
-    pi = 0;
-    sum = 0;
-    inicio = omp_get_wtime();
-    step = 1.0/(double) num_steps;
-    #pragma omp parallel private(x) num_threads(4)
+
+
+    double inicio2 = omp_get_wtime();
+
+    soma = 0;
+
+    #pragma omp parallel num_threads(3)
     {
-        #pragma omp for reduction (+:sum)
-        for (i=0;i< num_steps; i++){
-            x = (i + 0.5) * step; // Largura do retângulo
-            sum = sum + 4.0 / (1.0 + x*x); // Sum += Área do retângulo
-        }
-    }
-    pi = step * sum;
-    fim = omp_get_wtime();
-    double t_paralelo = fim-inicio;
-    double speedup = t_serial/t_paralelo;
-    printf("Pi: %f\n",pi);
-    printf("Execucao paralela(s): %f\n",t_paralelo);
-    printf("Speedup: %.4f\n", speedup);
-	printf("Eficiencia: %.4f\n",speedup/4.0);
-    return 0;
-    */
-
-    x = 0;
-    pi = 0;
-    sum = 0;
-    inicio = omp_get_wtime();
-    step = 1.0/(double) num_steps;
-    #pragma omp parallel private(x,sum) num_threads(4)
-    {
-
-        #pragma omp for
-        for (i=0;i< num_steps; i++){
-            x = (i + 0.5) * step; // Largura do retângulo
-            sum = sum + 4.0 / (1.0 + x*x); // Sum += Área do retângulo
-        }
-
-        #pragma omp critical
-        pi += sum * step;
-
+        #pragma omp for reduction (+:soma)
+            for(i = 1; i < num; i++) {
+                if((num % i) == 0) {
+                    soma += i;
+                }
+            }
     }
 
-    fim = omp_get_wtime();
-    double t_paralelo = fim-inicio;
-    double speedup = t_serial/t_paralelo;
-    printf("Pi: %f\n",pi);
-    printf("Execucao paralela(s): %f\n",t_paralelo);
-    printf("Speedup: %.4f\n", speedup);
-	printf("Eficiencia: %.4f\n",speedup/4.0);
+    double fim2 = omp_get_wtime();
+    double t_serial1 = fim2-inicio2;
+    printf("soma dos divisores igual à %d", soma);
+    printf("\nExecucao paralela(s): %f\n",t_serial1);
+
+    /*
+    FIM
+    */
     return 0;
+}
+
+double *gerar_vetor(int n) {
+    double *vetor;
+    int i;
+    vetor = (double *)malloc(sizeof(double) * n);
+    for (i=0;i<n;i++) {
+        double num = (rand() / (float)RAND_MAX);
+        vetor[i] = num;
+    }
+    return vetor;
+}
+
+int *gerar_vetor_inteiro(int n) {
+    int *vetor;
+    int i;
+    vetor = (int *)malloc(sizeof(int) * n);
+    for (i=0;i<n;i++) {
+        int num = (rand() % MAX);
+        vetor[i] = num;
+    }
+    return vetor;
+}
+
+void mostrar_vetor(double *v,int tamanho) {
+    int i;
+    for (i=0;i<tamanho;i++) {
+        printf("[%.5f]",v[i]);
+    }
+    printf("\n");
+}
+
+void mostrar_vetor_inteiro(int *v,int tamanho) {
+    int i;
+    for (i=0;i<tamanho;i++) {
+        printf("[%d]",v[i]);
+    }
+    printf("\n");
 }
